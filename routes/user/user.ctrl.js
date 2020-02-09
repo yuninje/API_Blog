067ada4e01430b5f0a46a9ecc7e5e97d245428ec
INJE YUN <dbsdlswp@naver.com>
2020-02-09 13:25:44 +0900

    user test 성공

--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -16,7 +16,7 @@ const getUsers = (req, res) => {
   };
 
   const onError = error => {
-    console.error(error);
+    //console.error(error);
     res.status(400);
   };
 
@@ -44,7 +44,7 @@ const getUser = (req, res) => {
   };
 
   const onError = error => {
-    console.error(error);
+    //console.error(error);
     res.status(400);
   };
 
@@ -71,19 +71,18 @@ const addUser = (req, res) => {
   };
 
   const onError = error => {
-    console.error(error);
+    //console.error(error);
     res.status(400).json(ErrorHandler(error.message));
   };
 
   models.sequelize
     .transaction(async t => {
+      if (!(user_id && user_pw && email && nick && name))
+        throw new Error(errors.BADREQ);
       const user = await User.findOne({
-        where: { 
-            [Op.or] : [
-                { user_id : user_id},
-                { nick : nick}
-            ]
-         },
+        where: {
+          [Op.or]: [{ user_id: user_id }, { nick: nick }]
+        },
         transaction: t
       });
       if (user) throw new Error(errors.EXIST);
@@ -106,12 +105,52 @@ const addUser = (req, res) => {
 };
 
 const updateUser = (req, res) => {
-  return;
+  const { user_id } = req.params;
+  const { name, email, nick, user_pw } = req.body;
+
+  const respond = () => {
+    res.json({
+      result: true,
+      data: "회원 정보 수정 성공"
+    });
+  };
+
+  const onError = error => {
+    //console.error(error);
+    res.status(400).json(ErrorHandler(error.message));
+  };
+
+  models.sequelize
+    .transaction(async t => {
+      let user = await User.findOne({
+        where: { user_id },
+        transaction: t
+      });
+      if (!user) throw new Error(errors.BADREQ);
+      user = await User.findOne({
+        where: { nick },
+        transaction: t
+      });
+      if (user) throw new Error(errors.EXIST);
+      await User.update(
+        {
+          user_pw,
+          email,
+          nick,
+          name
+        },
+        {
+          where: { user_id },
+          transaction: t
+        }
+      );
+    })
+    .then(respond)
+    .catch(onError);
 };
 
 const deleteUser = (req, res) => {
   const { user_id } = req.params;
-  let transaction;
 
   const respond = () => {
     res.json({
@@ -121,23 +160,20 @@ const deleteUser = (req, res) => {
   };
 
   const onError = error => {
-    console.error(error);
-    res.status(400);
-  };
-
-  const remove = user => {
-    if (!user) throw new Error(errors.BADREQ);
-    User.destroy({
-      where: { user_id }
-    });
+    //console.error(error);
+    res.status(400).json(ErrorHandler(error.message));
   };
 
   models.sequelize
-    .transaction(transaction => {
-      transaction = transaction;
-      return User.findOne({
+    .transaction(async t => {
+      transaction = t;
+      const user = await User.findOne({
         where: { user_id }
-      }).then(remove);
+      });
+      if (!user) throw new Error(errors.BADREQ);
+      await User.destroy({
+        where: { user_id }
+      });
     })
     .then(respond)
     .catch(onError);
--- routes/user/user.spec.js
+++ routes/user/user.spec.js
@@ -1,187 +1,191 @@
-const app = require('../../app')
-const should = require('should')
-const request = require('supertest')
+const app = require("../../app");
+const should = require("should");
+const request = require("supertest");
 
-const success = '성공'
-const fail = '실패'
+const success = "성공";
+const fail = "실패";
 
+describe("GET /users", () => {
+  describe(success, () => {
+    it("User 배열을 반환한다.", done => {
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          res.body.should.have.property("data").with.instanceOf(Array);
+          done();
+        });
+    });
 
+    it("최대 limit 갯수만큼 응답한다.", done => {
+      request(app)
+        .get("/users?limit=2")
+        .end((err, res) => {
+          res.body.should.have.property("data").with.lengthOf(2);
+          done();
+        });
+    }),
+      it("limit이 잘못 입력되거나 미입력되면 5개를 응답한다.", done => {
+        request(app)
+          .get("/users?limit=two")
+          .end((err, res) => {
+            res.body.should.have.property("data").with.lengthOf(5);
+            done();
+          });
+      });
+  });
+});
 
-describe('GET /users', () => {
-    describe(success, () =>{
-        it('User 배열을 반환한다.', done => {
-            request(app)
-                .get('/users')
-                .end((err, res) => {
-                    res.body.should.have.property('data').with.instanceOf(Array)
-                    done()
-                })
+describe("POST /users", () => {
+  const url = "users";
+  describe(success, () => {
+    it(`result : true, data : '회원 가입 성공'`, done => {
+      request(app)
+        .post("/users")
+        .send({
+          user_id: "dbsdlswp",
+          user_pw: "dbs123",
+          email: "dbsdlswp@naver.com",
+          nick: "코봉이",
+          name: "윤인제"
         })
+        .end((err, res) => {
+          res.body.should.have.property("result", true);
+          res.body.should.have.property("data", "회원 가입 성공");
+          done();
+        });
+    });
+  });
+  describe(fail, () => {
+    it("user_id 가 중복되면 409 응답, result : false, status : 409", done => {
+      request(app)
+        .post("/users")
+        .send({
+          user_id: "dbsdlswp",
+          user_pw: "dbs123",
+          email: "dbsdlswp@naver.com",
+          nick: "코봉이3121",
+          name: "윤인제1231"
+        })
+        .expect(400)
+        .end((err, res) => {
+          res.body.should.have.property("result", false);
+          res.body.should.have.property("status", 409);
+          done();
+        });
+    });
+    it("nick 가 중복되면 400 응답, result : false, status : 409", done => {
+      request(app)
+        .post("/users")
+        .send({
+          user_id: "dbsdlswp112313",
+          user_pw: "dbs121233",
+          email: "dbsdl123swp@naver.com",
+          nick: "aaaa",
+          name: "윤444인제"
+        })
+        .expect(400)
+        .end((err, res) => {
+          res.body.should.have.property("result", false);
+          res.body.should.have.property("status", 409);
+          done();
+        });
+    });
+    it("빠진 데이터가 있으면 400 응답한다.", done => {
+      request(app)
+        .post("/users")
+        .send({
+          user_id: "dbsdlswp1",
+          user_pw: "dbs123",
+          nick: "코봉이",
+          name: "윤인제"
+        })
+        .expect(400)
+        .end((err, res) => {
+            res.body.should.have.property('result', false)
+            res.body.should.have.property('status', 400)
+            done()
+        });
+    });
+  });
+});
 
-        it('최대 limit 갯수만큼 응답한다.', done => {
-            request(app)
-                .get('/users?limit=2')
-                .end((err, res) => {
-                    res.body.should.have.property('data').with.lengthOf(2)
-                    done()
-                })
-        }),
-        it('limit이 잘못 입력되거나 미입력되면 5개를 응답한다.', done => {
-            request(app)
-                .get( '/users?limit=two' )
-                .end((err, res) => {
-                    res.body.should.have.property('data').with.lengthOf(5)
-                    done()
-                })
+describe("UPDATE /users/:user_id", () => {
+  describe(success, () => {
+    it(`result : true, data : '회원 정보 수정 성공'`, done => {
+      request(app)
+        .put("/users/dbsdlswp")
+        .send({
+          user_pw: "1234",
+          email: "aaaaaa@aaa.com",
+          nick: "asdfasfd",
+          name: "spdpdpdla"
         })
-    })
-})
+        .end((err, res) => {
+          res.body.should.have.property("result", true);
+          res.body.should.have.property("data", "회원 정보 수정 성공");
+          done();
+        });
+    });
+  });
+  describe(fail, () => {
+    it(`해당 id의 유저가 존재하지 않으면 400을 응답하고, result : false, status : 400`, done => {
+      request(app)
+        .put("/users/aaaaabbbbb11")
+        .send({
+          user_pw: "1234",
+          email: "aaaaaa@aaa.com",
+          nick: "asdfasfd",
+          name: "spdpdpdla"
+        })
+        .expect(400)
+        .end((err, res) => {
+          res.body.should.have.property("result", false);
+          res.body.should.have.property("status", 400);
+          done();
+        });
+    });
+    it("nick 이 중복되면 400을 응답하고, result : false, status : 409", done => {
+      request(app)
+        .put("/users/dbsdlswp")
+        .send({
+          user_pw: "1234",
+          email: "aaaaaa@aaa.com",
+          nick: "test",
+          name: "spdpdpdla"
+        })
+        .expect(400)
+        .end((err, res) => {
+          res.body.should.have.property("result", false);
+          res.body.should.have.property("status", 409);
+          done();
+        });
+    });
+  });
+});
 
-describe('POST /users', () => {
-    const url = 'users'
-    describe(success, () =>{
-        it(`result : true, data : '회원 가입 성공'`, done => {
-            request(app)
-                .post('/users')
-                .send({
-                    'user_id' : 'dbsdlswp',
-                    'user_pw' : 'dbs123',
-                    'email' : 'dbsdlswp@naver.com',
-                    'nick' : '코봉이',
-                    'name' : '윤인제'
-                })
-                .end((err, res) => {
-                    res.body.should.have.property('result', true)
-                    res.body.should.have.property('data', '회원 가입 성공')
-                    done()
-                })
-        })
-    })
-    describe(fail, () => {
-        it('user_id 가 중복되면 409 응답한다.', done => {
-            request(app)
-                .post('/users')
-                .send({
-                    'user_id' : 'dbsdlswp',
-                    'user_pw' : 'dbs123',
-                    'email' : 'dbsdlswp@naver.com',
-                    'nick' : '코봉이3121',
-                    'name' : '윤인제1231'
-                })
-                .expect(400)
-                .end((err, res) => {
-                    res.body.should.have.property('result', false)
-                    res.body.should.have.property('status', 409)
-                    res.body.should.have.property('message', 'Conflict')
-                    done()
-                })
-        })
-        it('nick 가 중복되면 400 응답한다.',done => {
-            request(app)
-                .post('/users')
-                .send({
-                    'user_id' : 'dbsdlswp112313',
-                    'user_pw' : 'dbs121233',
-                    'email' : 'dbsdl123swp@naver.com',
-                    'nick' : 'aaaa',
-                    'name' : '윤444인제'
-                })
-                .expect(400)
-                .end((err, res) => {
-                    console.log('nick 중복 err')
-                    res.body.should.have.property('result', false)
-                    res.body.should.have.property('status', 409)
-                    res.body.should.have.property('message', 'Conflict')
-                    done()
-                })
-        })
-        it('빠진 데이터가 있으면 400 응답한다.', done => {
-            request(app)
-                .post('/users')
-                .send({
-                    'user_id' : 'dbsdlswp1',
-                    'user_pw' : 'dbs123',
-                    'nick' : '코봉이',
-                    'name' : '윤인제'
-                })
-                .expect(400)
-                .end(done)
-        })
-
-    })
-})
-
-describe('UPDATE /users/:id', () => {
-    describe(success, () => {
-        it('수정된 유저 객체를 반환한다.', done => {
-            request(app)
-                .put('/users/dbsdlswp')
-                .send({
-                    'user_pw' : '1234',
-                    'email' : 'aaaaaa@aaa.com',
-                    'nick' : 'asdfasfd',
-                    'name' : 'spdpdpdla'
-                })
-                .end((err, res) => {
-                    res.body.should.have.property('user_id')
-                    res.body.should.have.property('user_pw')
-                    res.body.should.have.property('email')
-                    res.body.should.have.property('nick')
-                    res.body.should.have.property('name')
-                    done()
-                })
-        })        
-    })
-    describe(fail, () => {
-        it('해당 id의 유저가 존재하지 않으면 400을 응답한다.', done => {
-            request(app)
-                .put('/users/aaaaabbbbb11')
-                .send({
-                    'user_pw' : '1234',
-                    'email' : 'aaaaaa@aaa.com',
-                    'nick' : 'asdfasfd',
-                    'name' : 'spdpdpdla'
-                })
-                .expect(400)
-                .end(done)
-        })
-        it('nick 이 중복되면 400을 응답한다.', done => {
-            request(app)
-                .put('/users/dbsdlswp')
-                .send({
-                    'user_pw' : '1234',
-                    'email' : 'aaaaaa@aaa.com',
-                    'nick' : '코봉이32123',
-                    'name' : 'spdpdpdla'
-                })
-                .expect(400)
-                .end(done)
-        })
-    })
-})
-
-describe('DELETE /users/:id', () => {
-    describe(success, () => {
-        it(`data : '회원 탈퇴 성공', result : true`, done => {
-            request(app)
-                .delete('/users/dbsdlswp')
-                .end((err, res) => {
-                    res.body.should.have.property('result', true)
-                    res.body.should.have.property('data', '회원 탈퇴 성공')
-                    done()
-                })
-        })
-    })
-    describe(fail, () => {
-        it('해당 id의 유저가 존재하지 않으면 400을 응답한다.', done => {
-            request(app)
-                .delete('/users/adfafdsf321sfd')
-                .end((err, res) => {
-                    res.body.should.have.property('result', false)
-                    res.body.should.have.property('status', 400)
-                    done()
-                })
-        })
-    })
-})
-
+describe("DELETE /users/:user_id", () => {
+  describe(success, () => {
+    it(`result : true, data : '회원 탈퇴 성공'`, done => {
+      request(app)
+        .delete("/users/dbsdlswp")
+        .expect(400)
+        .end((err, res) => {
+          res.body.should.have.property("result", true);
+          res.body.should.have.property("data", "회원 탈퇴 성공");
+          done();
+        });
+    });
+  });
+  describe(fail, () => {
+    it("해당 id의 유저가 존재하지 않으면 400을 응답하고, result : false, status : 400", done => {
+      request(app)
+        .delete("/users/adfafdsf321sfd")
+        .expect(400)
+        .end((err, res) => {
+          res.body.should.have.property("result", false);
+          res.body.should.have.property("status", 400);
+          done();
+        });
+    });
+  });
+});
